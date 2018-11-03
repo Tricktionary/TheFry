4c3fc31887243d4a493776dbced4bdeb3b5e4e93
Tricktionaries <andyaoux@hotmail.com>
2018-11-03 15:46:39 -0400

    Need to work on download button

--- js/main.js
+++ js/main.js
@@ -1,20 +1,86 @@
 $(document).ready(function(){
+
+    //Initial Draw
+    $("#brightness_value").text($("#brightness").val());
+    $("#contrast_value").text($("#contrast").val());
+    $("#saturation_value").text($("#saturation").val());
+    $("#sharpen_value").text($("#sharpen").val());
+    $("#noise_value").text($("#noise").val());
     
+
     var canvas = $("#canvas")[0];
     var ctx = canvas.getContext("2d");
     var img = new Image();
-
+    var image;
+    
     //Opens File Modal
     $("#submit").click(function(){
         $("#fry_file").click();
-    })
+    });
 
     //Upload The File to the canvas 
     $("#fry_file").change(function(event){
         var files = event.target.files;
-        var image = files[0];
+        image = files[0];
+        uploadPicture(image)
+    });
+
+    //Download Button
+    $("#download_button").click(function(event){
+        var imageToDownload = canvas.toDataURL("image/png");
+        $("#download_button").href=imageToDownload;
+    });
+
+    //Fries Picture
+    $("#fry_button").click(function(){
+        /* Pull Values */
+        var brightness = $("#brightness").val();
+        var contrast = $("#contrast").val();
+        var saturation = $("#saturation").val();
+        var sharpen = $("#sharpen").val();
+        var noise = $("#noise").val();
+
+        Caman('#canvas', function () {
+            this.brightness(brightness);
+            this.contrast(contrast);
+            this.saturation(saturation);
+            this.sharpen(sharpen);
+            this.noise(noise);
+            this.render();
+        });
+    });
+
+    //Changes Text Value
+    $("input").change(function(){
+        $("#brightness_value").text($("#brightness").val());
+        $("#contrast_value").text($("#contrast").val());
+        $("#saturation_value").text($("#saturation").val());
+        $("#sharpen_value").text($("#sharpen").val());
+        $("#noise_value").text($("#noise").val());
+    })
+
+    //Reset The Image
+    $("#reset_button").click(function(){
+        Caman("#canvas",function(){
+            this.reset();
+        });
+        $("#brightness").val(0);
+        $("#contrast").val(0);
+        $("#saturation").val(0);
+        $("#sharpen").val(0);
+        $("#noise").val(0);
+        $("#brightness_value").text($("#brightness").val());
+        $("#contrast_value").text($("#contrast").val());
+        $("#saturation_value").text($("#saturation").val());
+        $("#sharpen_value").text($("#sharpen").val());
+        $("#noise_value").text($("#noise").val());
+    })
+    
+    //Upload Image to Canvas
+    function uploadPicture(image){
         if(image.type.match("image.*")){
             $("#editArea").removeClass("hidden");
+            $("#editToggle").removeClass("hidden");
             var reader = new FileReader();
             reader.readAsDataURL(image);
             reader.onload = function(evt){
@@ -22,11 +88,11 @@ $(document).ready(function(){
                     img.src = evt.target.result;
 				    img.onload = () => ctx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
                         0, 0, canvas.width, canvas.height);
-			    }
+                }
             }
         }   
         else{
             alert("not an image");
         }
-    })
+    }
 })
